44e7768e0f7303a8790329084f578621e4a0442d
Vick-17 <vega.victor.0712@gmail.com>
2023-07-26 09:05:05 +0200

    Blague citation et enigme ajouter

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { getDaily } from '../Services/apiService';
+import React, { useEffect, useState } from "react";
+import { getDaily } from "../Services/apiService";
 
 const Main = () => {
   const [content, setContent] = useState(null);
+  const [showAnswerBlagues, setShowAnswerBlagues] = useState([]);
+  const [showAnswerEnigmes, setShowAnswerEnigmes] = useState([]);
 
   useEffect(() => {
     const fetchDailyContent = async () => {
       try {
         const data = await getDaily();
         setContent(data);
+        // Initialize the showAnswer arrays with false for each question
+        setShowAnswerBlagues(new Array(data.blagues.length).fill(false));
+        setShowAnswerEnigmes(new Array(data.enigme.length).fill(false));
       } catch (error) {
         console.error(error);
       }
@@ -17,25 +22,69 @@ const Main = () => {
     fetchDailyContent();
   }, []);
 
+  const handleShowAnswerBlague = (index) => {
+    // Create a copy of the showAnswerBlagues array and update the value for the selected question
+    const updatedShowAnswerBlagues = [...showAnswerBlagues];
+    updatedShowAnswerBlagues[index] = true;
+    setShowAnswerBlagues(updatedShowAnswerBlagues);
+  };
+
+  const handleShowAnswerEnigme = (index) => {
+    // Create a copy of the showAnswerEnigmes array and update the value for the selected question
+    const updatedShowAnswerEnigmes = [...showAnswerEnigmes];
+    updatedShowAnswerEnigmes[index] = true;
+    setShowAnswerEnigmes(updatedShowAnswerEnigmes);
+  };
+
   return (
     <div className="jqr-container">
       <div className="jqr-content">
         {content ? (
           <div>
-            <p>
-              <strong>Blague du jour :</strong>
-              <br />
-              {content.joke}
-            </p>
-            <p>
-              <strong>Citation du jour :</strong>
-              <br />
-              {content.quote}
-            </p>
-            <p>
-              <strong>Énigme du jour :</strong>
-              <br />
-            </p>
+            <p className="jqr-category-header">Blague du jour :</p>
+            <div className="jqr-category-divider"></div>
+            {content.blagues.map((blague, index) => (
+              <div key={blague.id}>
+                <p>{blague.text}</p>
+                {showAnswerBlagues[index] && blague.answer && (
+                  <div>
+                    <strong>Réponse :</strong> {blague.answer}
+                  </div>
+                )}
+                {/* Affichez d'autres propriétés de l'objet blague si nécessaire */}
+                {!showAnswerBlagues[index] && (
+                  <button onClick={() => handleShowAnswerBlague(index)}>
+                    Afficher la réponse
+                  </button>
+                )}
+              </div>
+            ))}
+            <p className="jqr-category-header">Citation du jour :</p>
+            <div className="jqr-category-divider"></div>
+            {content.citations.map((citation) => (
+              <div key={citation.id}>
+                <p>{citation.text}</p>
+                {citation.auteur && <p>Auteur : {citation.auteur}</p>}
+              </div>
+            ))}
+            <p className="jqr-category-header">Énigme du jour :</p>
+            <div className="jqr-category-divider"></div>
+            {content.enigme.map((enigme, index) => (
+              <div key={enigme.id}>
+                <p>{enigme.text}</p>
+                {showAnswerEnigmes[index] && enigme.answer && (
+                  <div>
+                    <strong>Réponse :</strong> {enigme.answer}
+                  </div>
+                )}
+                {/* Affichez d'autres propriétés de l'objet enigme si nécessaire */}
+                {!showAnswerEnigmes[index] && (
+                  <button onClick={() => handleShowAnswerEnigme(index)}>
+                    Afficher la réponse
+                  </button>
+                )}
+              </div>
+            ))}
           </div>
         ) : (
           <p>Contenu du jour non disponible.</p>
