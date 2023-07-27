e33a7ffe63fa39cc0ba8f1a44d8616374ecb0b14
Vick-17 <vega.victor.0712@gmail.com>
2023-07-27 12:00:49 +0200

    Delete blague et citation, ajout artiste

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -1,39 +1,31 @@
 import React, { useEffect, useState } from "react";
-import { getDaily } from "../Services/apiService";
+import { getDaily, getArtistOfDay } from "../Services/apiService";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   const [content, setContent] = useState(null);
-  const [showAnswerBlagues, setShowAnswerBlagues] = useState([]);
-  const [showAnswerEnigmes, setShowAnswerEnigmes] = useState([]);
+  const [showAnswer, setShowAnswer] = useState([]);
 
   useEffect(() => {
-    const fetchDailyContent = async () => {
+    const fetchData = async () => {
       try {
-        const data = await getDaily();
-        setContent(data);
-        // Initialize the showAnswer arrays with false for each question
-        setShowAnswerBlagues(new Array(data.blagues.length).fill(false));
-        setShowAnswerEnigmes(new Array(data.enigme.length).fill(false));
+        const dailyContent = await getDaily();
+        setContent(dailyContent);
+        setShowAnswer(new Array(dailyContent.blagues.length).fill(false));
+        const artist = await getArtistOfDay();
+        setContent((prevContent) => ({ ...prevContent, artist }));
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchDailyContent();
+    fetchData();
   }, []);
 
-  const handleShowAnswerBlague = (index) => {
-    // Create a copy of the showAnswerBlagues array and update the value for the selected question
-    const updatedShowAnswerBlagues = [...showAnswerBlagues];
-    updatedShowAnswerBlagues[index] = true;
-    setShowAnswerBlagues(updatedShowAnswerBlagues);
-  };
-
-  const handleShowAnswerEnigme = (index) => {
-    // Create a copy of the showAnswerEnigmes array and update the value for the selected question
-    const updatedShowAnswerEnigmes = [...showAnswerEnigmes];
-    updatedShowAnswerEnigmes[index] = true;
-    setShowAnswerEnigmes(updatedShowAnswerEnigmes);
+  const handleShowAnswer = (index) => {
+    const updatedShowAnswer = [...showAnswer];
+    updatedShowAnswer[index] = true;
+    setShowAnswer(updatedShowAnswer);
   };
 
   return (
@@ -41,50 +33,35 @@ const Main = () => {
       <div className="jqr-content">
         {content ? (
           <div>
-            <p className="jqr-category-header">Blague du jour :</p>
-            <div className="jqr-category-divider"></div>
-            {content.blagues.map((blague, index) => (
-              <div key={blague.id}>
-                <p>{blague.text}</p>
-                {showAnswerBlagues[index] && blague.answer && (
-                  <div>
-                    <strong>Réponse :</strong> {blague.answer}
-                  </div>
-                )}
-                {/* Affichez d'autres propriétés de l'objet blague si nécessaire */}
-                {!showAnswerBlagues[index] && (
-                  <button onClick={() => handleShowAnswerBlague(index)}>
-                    Afficher la réponse
-                  </button>
-                )}
-              </div>
-            ))}
-            <p className="jqr-category-header">Citation du jour :</p>
-            <div className="jqr-category-divider"></div>
-            {content.citations.map((citation) => (
-              <div key={citation.id}>
-                <p>{citation.text}</p>
-                {citation.auteur && <p>Auteur : {citation.auteur}</p>}
-              </div>
-            ))}
+            {/* Énigmes */}
             <p className="jqr-category-header">Énigme du jour :</p>
             <div className="jqr-category-divider"></div>
             {content.enigme.map((enigme, index) => (
               <div key={enigme.id}>
                 <p>{enigme.text}</p>
-                {showAnswerEnigmes[index] && enigme.answer && (
-                  <div>
-                    <strong>Réponse :</strong> {enigme.answer}
-                  </div>
-                )}
-                {/* Affichez d'autres propriétés de l'objet enigme si nécessaire */}
-                {!showAnswerEnigmes[index] && (
-                  <button onClick={() => handleShowAnswerEnigme(index)}>
+                {showAnswer[index] && <p>{enigme.answer}</p>}
+                {!showAnswer[index] && (
+                  <button onClick={() => handleShowAnswer(index)}>
                     Afficher la réponse
                   </button>
                 )}
               </div>
             ))}
+            {/* Artiste du jour */}
+            <div className="jqr-artist-section">
+              <p className="jqr-category-header">Artiste du jour :</p>
+              <div className="jqr-category-divider"></div>
+              {content && content.artist && (
+                <div>
+                  {content.artist.photo && (
+                    <img src={content.artist.photo} alt="Artiste du jour" />
+                  )}
+                  <p>Nom : {content.artist.name}</p>
+                  <p>Ville : {content.artist.city}</p>
+                  <p>Site : <Link to={`${content.artist.website}`} >{content.artist.website}</Link> </p>
+                </div>
+              )}
+            </div>
           </div>
         ) : (
           <p>Contenu du jour non disponible.</p>
